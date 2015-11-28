6eada3dd2c674d64f077937d4fe2661981608314
Justin Harris <justin.harris@wpengine.com>
2015-11-28 16:47:19 -0600

    Bob

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,8 +1,11 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
-
+      String.ends_with? input, "?" -> "Sure."
+      String.length(String.strip(input)) == 0 -> "Fine. Be that way!"
+      # This is kind of weird, but it makes sure that there are alpha chars
+      String.upcase(input) == input && String.downcase(input) != input -> "Whoa, chill out!"
+      true -> "Whatever."
     end
   end
 end
