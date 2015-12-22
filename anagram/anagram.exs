752b0b9d063e09132d59c7a22586a75102e59b4b
Justin Harris <justin.harris@wpengine.com>
2015-12-22 15:46:46 -0600

    Anagrams

--- anagram/anagram.exs
+++ anagram/anagram.exs
@@ -2,8 +2,23 @@ defmodule Anagram do
   @doc """
   Returns all candidates that are anagrams of, but not equal to, 'base'.
   """
+
+  @spec letters(String.t) :: [String.t]
+  def letters(word) do
+    word
+    |> String.graphemes
+    |> Enum.map(&String.downcase/1)
+    |> Enum.sort
+  end
+
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
-  
+    base_lowered = String.downcase(base)
+    base_letters = letters(base)
+
+    Enum.filter(candidates, fn candidate ->
+      base_lowered != String.downcase(candidate) &&
+      base_letters == letters(candidate)
+    end)
   end
 end
