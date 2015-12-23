5233eafcd059df4adaa722a02373057ed4bffffb
Justin Harris <justin.harris@wpengine.com>
2015-12-23 16:37:38 -0500

    Point mutations

--- point-mutations/point_mutations.exs
+++ point-mutations/point_mutations.exs
@@ -7,7 +7,20 @@ defmodule DNA do
   iex> DNA.hamming_distance('AAGTCATA', 'TAGCGATC')
   4
   """
+  @spec hamming_distance([], []) :: non_neg_integer
+  def hamming_distance([], []), do: 0
+
+  @spec hamming_distance([char], [char]) :: non_neg_integer
+  def hamming_distance([s | strand1], [s | strand2]) do
+    hamming_distance(strand1, strand2)
+  end
+
   @spec hamming_distance([char], [char]) :: non_neg_integer
   def hamming_distance(strand1, strand2) do
+    if Enum.count(strand1) == Enum.count(strand2) do
+      1 + hamming_distance(tl(strand1), tl(strand2))
+    else
+      nil
+    end
   end
 end
